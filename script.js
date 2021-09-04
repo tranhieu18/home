db3b93855ac3a84903b73074a8d8722b254ae2b2
tranhieu18 <hieutlthe161328@fpt.edu.vn>
2021-09-04 19:23:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const setProperty = (duration) => {
+  document.documentElement.style.setProperty(
+    "--animation-time",
+    duration + "s"
+  );
+};
+ 
+const changeAnimationTime = () => {
+  const animationDuration = Math.random();
+  setProperty(animationDuration);
+};
+ 
+setInterval(changeAnimationTime, 1000);
